7d9aec74d6da6da7232ceb6db93aa92cdc6d6acd
imdaano <danisanchezpsc@gmail.com>
2024-05-24 09:04:42 +0200

    first Spring Project

--- src/main/java/edu/daniel/projectJPA/entities/Animal.java
+++ src/main/java/edu/daniel/projectJPA/entities/Animal.java
@@ -9,6 +9,61 @@ public class Animal {
     private String especie;
     private Double peso;
     private Integer edad;
-    
 
+    public Animal() {}
+
+    public Animal(Long id, String nombre, String especie, Double peso, Integer edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.peso = peso;
+        this.edad = edad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    
+    
 }
+
+
+
+
